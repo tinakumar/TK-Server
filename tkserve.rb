7ee541ca32f450ff3d25a100c9e455fd9d4bd66e
Tina Kumar <tinakumar07@gmail.com>
2013-11-05 18:35:36 -0800

    made server more complex with security

--- tkserve.rb
+++ tkserve.rb
@@ -1,17 +1,69 @@
 require 'socket'
+require 'uri'
+
+WEB_ROOT = './public'
+
+CONTENT_TYPE_MAPPING = {
+  'html' => 'text/html',
+  'txt' => 'text/plain',
+  'png' => 'image/png',
+  'jpg' => 'image/jpeg'
+}
+
+DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+def requested_file(request_line)
+  request_uri = request_line.split(" ")[1]
+  path        = URI.unescape(URI(request_uri).path)
+
+  clean = []
+
+  parts = path.split("/")
+
+  parts.each do |part|
+    next if part.empty? || part == '.'
+    part == '..' ? clean.pop : clean << part
+  end
+
+  File.join(WEB_ROOT, *clean)
+end
 
 server = TCPServer.new('localhost', 2345)
 
 loop do
   socket = server.accept
-  request = socket.gets
-  STDERR.puts request
-  response = "Hello World!\n"
-  socket.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Tyle: text/plain\r\n" +
+  request_line = socket.gets
+
+  STDERR.puts request_line
+
+  path = requested_file(request_line)
+
+  path = File.join(path, 'index.html') if File.directory?(path)
+
+  if File.exist?(path) && !File.directory?(path)
+    File.open(path, "rb") do |file|
+      socket.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
                "Content-Length: #{response.bytesize}\r\n" +
                "Connection: close\r\n"
-  socket.print "\r\n"
-  socket.print response
+      socket.print "\r\n"
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
+
+    socket.print "HTTP/1.1 404 Not Found" +
+              "Content-Type: text/plain\r\n" +
+              "Content-Length: #{message.size}\r\n" +
+              "Connection: close\r\n"
+    socket.print "\r\n"
+    socket.print message
+  end
+
   socket.close
 end
